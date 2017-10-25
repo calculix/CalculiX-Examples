fd3042bde89466800c00fbaaf1355e8b08afd325
Martin Kraska <kraska@fh-brandenburg.de>
2017-10-25 22:20:11 +0200

    add test.py

---
+++ Streifen/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import os
+
+os.system("cgx -b b.fbd")
+os.system("cgx -b sh.fbd")
+os.system("cgx -b sm.fbd")
+os.system("cgx -b sr.fbd")
+os.system("cgx -b scd.fbd")
+os.system("cgx -b sck.fbd")
+
+
