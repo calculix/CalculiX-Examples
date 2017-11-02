f8fdf114d72f8ded1da409a9a8fc231b532bdaf5
Martin Kraska <kraska@fh-brandenburg.de>
2017-11-02 20:08:51 +0100

    test with ccx 2.13, add test.py

---
+++ Linear/L-Plate/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+import os
+
+os.system("param.py par.run.fbl")
+os.system("cgx -b run.fbl")
+os.system("ccx solve")
+os.system("cgx -b vmodes.fbd")
+os.system("cgx -b bmodes.fbd")
