7e43f771f6cf57d76094153e9e912c58e74c4396
Martin Kraska <kraska@fh-brandenburg.de>
2018-01-01 19:46:17 +0100

    fix test script

--- Test/BeamSections/test.py
+++ Test/BeamSections/test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 import os
-os.system("ccx u1general")
-os.system("sed -n '4,14 p' u1general.dat > u1.txt")
+os.system("ccx u1General")
+os.system("sed -n '4,14 p' u1General.dat > u1.txt")
 os.system("gnuplot u1.plt")
