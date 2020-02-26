857f02f258b241f6b396b9df99b0afd4fd4fad7e
imirzov <imirzov@gmail.com>
2020-02-26 18:46:14 +0100

    Separate output

--- Contact/Shell0/test.py
+++ Contact/Shell0/test.py
@@ -17,6 +17,22 @@ def modify_path():
     print '\nPATH = {}\n'.format(os.environ['PATH'])
 
 
+# Move new files and folders to 'Refs'
+def move(old_snap):
+    new_snap = os.listdir(os.curdir)
+    if not os.path.exists('Refs'):
+        os.mkdir('Refs')
+    for f in new_snap:
+        if not f in old_snap:
+            fname = os.path.basename(f)
+            new_name = os.path.join(os.curdir, 'Refs', fname)
+            if os.path.isfile(new_name):
+                os.remove(new_name)
+            if os.path.isdir(new_name):
+                shutil.rmtree(new_name)
+            os.rename(f, new_name)
+
+
 def run():
     etypes = ("qu4", "qu8", "qu8r")
     ctypes = ("tie", "equ", "pc-ns", "pc-ss")
@@ -57,21 +73,6 @@ def run():
                 r.write(" | " + "{0:8.3g}".format(freq[i,fcol]))
             r.write("\n")
     r.close()
-
-# Move new files and folders to 'Refs'
-def move(old_snap):
-    new_snap = os.listdir(os.curdir)
-    if not os.path.exists('Refs'):
-        os.mkdir('Refs')
-    for f in new_snap:
-        if not f in old_snap:
-            fname = os.path.basename(f)
-            new_name = os.path.join(os.curdir, 'Refs', fname)
-            if os.path.isfile(new_name):
-                os.remove(new_name)
-            if os.path.isdir(new_name):
-                shutil.rmtree(new_name)
-            os.rename(f, new_name)
 
 
 if __name__ == '__main__':
