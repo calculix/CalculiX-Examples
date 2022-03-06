c9912ab62d16aabc324cbbef75627434bb368dc4
Martin Kraska <martin.kraska@th-brandenburg.de>
2022-03-06 22:14:58 +0000

    upgrade to python 3

--- Drahtbiegen/Biegung/Biegung.py
+++ Drahtbiegen/Biegung/Biegung.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-import pylab
+import matplotlib.pyplot as plt
 import numpy
 
 de = numpy.genfromtxt("total internal energy_EDRAHT.txt")
 dm = numpy.genfromtxt("forces fx,fy,fz_NROT.txt")
-pylab.plot(de[:,0],de[:,1],'b',dm[:,0],dm[:,3],'r')
-pylab.grid(True)
-pylab.xlim([0,1])
-pylab.xlabel("t")
-pylab.ylabel("y")
-pylab.legend(["Energy","Moment"],loc=0)
-pylab.savefig("Biegung-history")
+plt.plot(de[:,0],de[:,1],'b',dm[:,0],dm[:,3],'r')
+plt.grid(True)
+plt.xlim([0,1])
+plt.xlabel("t")
+plt.ylabel("y")
+plt.legend(["Energy","Moment"],loc=0)
+plt.savefig("Biegung-history")
--- Drahtbiegen/Biegung/test.py
+++ Drahtbiegen/Biegung/test.py
@@ -16,67 +16,6 @@ def modify_path():
         os.environ['PATH'] += os.pathsep + scripts_dir
     print('\nPATH = {}\n'.format(os.environ['PATH']))
 
-
-# preprocessing
-def pre():
-    toRemovePre = (
-        "*.nam",
-        "*.bou",
-        "*.sur",
-        "hcpy*.png",
-        "symy.png",
-        "wfix.png",
-        "parts.png",
-        "contact.png",
-        "all.msh",
-        "rb1.inp")
-
-    print("removing files")
-    for spec in toRemovePre:
-        files=glob.glob(spec)
-        for f in files:
-            print(f)
-            os.remove(f)
-    os.system("cgx -b pre.fbd")
-
-
-# solve, can take a while
-def solve():
-    try: os.remove("Biegung.frd")
-    except: pass
-    try: os.remove("Biegung.dat")
-    except: pass
-    try: os.remove("Biegung.sta")
-    except: pass
-    try: os.remove("Biegung.cvg")
-    except: pass
-    os.system("ccx Biegung >>Biegung.log")
-
-
-# convergence plot, reaction-time-plot
-def post():
-    try:
-        os.remove("Biegung.png")
-    except:
-        0
-    os.remove("Biegung-history.png")
-    files=glob.glob("force*.txt")
-    for f in files:
-        os.remove(f)
-    files=glob.glob("total*.txt")
-    for f in files:
-            os.remove(f)
-    os.remove("movie.gif")
-    os.remove("deform.png")
-    os.remove("PE.png")
-
-    os.system("monitor.py Biegung")
-    os.system("dat2txt.py Biegung")
-    os.system("./Biegung.py")
-    os.system("cgx -b Animation.fbd")
-    os.system("cgx -b post.fbd")
-
-
 # Move new files and folders to 'Refs'
 def move(old_snap):
     new_snap = os.listdir(os.curdir)
@@ -104,7 +43,11 @@ if __name__ == '__main__':
     # Run the example
     modify_path()
     snap = os.listdir(os.curdir)
-    pre()
-    solve()
-    post()
+    os.system("cgx -b pre.fbd")
+    os.system("ccx Biegung >>Biegung.log")
+    os.system("monitor.py Biegung")
+    os.system("dat2txt.py Biegung")
+    os.system("./Biegung.py")
+    os.system("cgx -b Animation.fbd")
+    os.system("cgx -b post.fbd")
     #move(snap)
